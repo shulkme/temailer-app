e9859d3a677ada19551445bfd6df05504ef2fa2a
shulksteve <shulk.work@gmail.com>
2025-09-25 17:38:56 +0800

    up

--- src/app/[locale]/(app)/domain/components/release-modal.tsx
+++ src/app/[locale]/(app)/domain/components/release-modal.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { releaseDomain } from '@/apis/domain';
 import { useRequest } from 'ahooks';
 import { App, Button, Modal, Result } from 'antd';
+import Confetti from 'canvas-confetti';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const ReleaseModal: React.FC<{
   open: boolean;
@@ -14,6 +14,7 @@ const ReleaseModal: React.FC<{
   afterClose?: () => void;
 }> = ({ open, setOpen, id, afterSubmit, afterClose }) => {
   const [success, setSuccess] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [salvage, setSalvage] = useState(0);
   const { message } = App.useApp();
   const t = useTranslations('app.pages.domain.release');
@@ -26,13 +27,25 @@ const ReleaseModal: React.FC<{
 
   const { loading: submitting, run: doSubmit } = useRequest(
     async () => {
-      return await releaseDomain(id!);
+      //return await releaseDomain(id!);
+      return { data: 100 };
     },
     {
       manual: true,
       onSuccess: (res) => {
         setSuccess(true);
         setSalvage(res.data);
+        if (!canvasRef.current) return;
+        const myConfetti = Confetti.create(canvasRef.current, { resize: true });
+        myConfetti({
+          particleCount: 100, // 例子数量
+          scalar: 1.5, // 粒子大小比例
+          spread: 180, // 扩散角度
+          origin: { y: -0.1 },
+          startVelocity: -35, // 初速度
+          gravity: 2, // 重力
+          ticks: 100, // 粒子寿命
+        });
       },
       onError: (e) => {
         message.error(e.message);
@@ -54,6 +67,18 @@ const ReleaseModal: React.FC<{
       afterClose={handleAfterClose}
       onCancel={() => setOpen(false)}
     >
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          pointerEvents: 'none', // 不阻止点击
+          zIndex: 10000, // 保证在 Modal 上层
+        }}
+      />
       {success ? (
         <>
           <Result
