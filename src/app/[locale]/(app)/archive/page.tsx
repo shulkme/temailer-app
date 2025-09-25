d5e957020aa6ac0afdc602ea4dd442a6630ee157
shulksteve <shulk.work@gmail.com>
2025-09-25 15:53:56 +0800

    up

--- src/app/[locale]/(app)/archive/page.tsx
+++ src/app/[locale]/(app)/archive/page.tsx
@@ -1,17 +1,22 @@
 'use client';
-import { getArchiveList } from '@/apis/archive';
+import { delArchive, getArchiveList } from '@/apis/archive';
 import { ArchiveRecord } from '@/apis/archive/types';
 import { AntdForm, AntdFormItem, AntdInput } from '@/components/antd';
 import { Title } from '@/providers/title';
 import { RiSearchLine } from '@remixicon/react';
-import { useAntdTable } from 'ahooks';
-import { Button, Card, FormProps, Space, Table } from 'antd';
+import { useAntdTable, useRequest } from 'ahooks';
+import { App, Button, Card, FormProps, Space, Table } from 'antd';
 import { useTranslations } from 'next-intl';
+import React, { useCallback, useState } from 'react';
 
 export default function Page() {
   const t = useTranslations('app.pages.archive');
+  const g = useTranslations('global');
   const [form] = AntdForm.useForm();
-  const { tableProps, search } = useAntdTable(
+  const { message, modal } = App.useApp();
+  const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
+
+  const { tableProps, search, refresh } = useAntdTable(
     async ({ current, pageSize }, params) => {
       return await getArchiveList({
         page: current,
@@ -31,9 +36,37 @@ export default function Page() {
 
   const { submit } = search;
 
+  const { runAsync: doDelete } = useRequest(delArchive, {
+    manual: true,
+    onSuccess: () => {
+      refresh();
+      setSelectedKeys([]);
+      message.success(g('response.success'));
+    },
+    onError: (e) => {
+      message.error(e.message);
+    },
+  });
+
   const onFormValuesChange: FormProps['onValuesChange'] = (changedValues) => {
     if (!Object.keys(changedValues).includes('name')) submit();
   };
+
+  const handleDelete = useCallback(
+    (ids: React.Key[]) => {
+      modal.confirm({
+        title: t('delete.title'),
+        content: t('delete.content'),
+        okText: t('delete.actions.ok'),
+        okType: 'danger',
+        onOk: async () => {
+          return await doDelete(ids as number[]);
+        },
+      });
+    },
+    [doDelete, modal, t],
+  );
+
   return (
     <>
       <Title title={t('title')} />
@@ -59,6 +92,13 @@ export default function Page() {
             </div>
             <div>
               <Space size="middle">
+                <Button
+                  danger
+                  disabled={!selectedKeys.length}
+                  onClick={() => handleDelete(selectedKeys)}
+                >
+                  {t('table.actions.delete')}
+                </Button>
                 <Button type="primary">{t('table.actions.export')}</Button>
               </Space>
             </div>
@@ -83,8 +123,27 @@ export default function Page() {
               },
               {
                 title: t('table.columns.operate'),
+                width: 140,
+                align: 'right',
+                render: (_, record) => {
+                  return (
+                    <Button
+                      size="small"
+                      type="link"
+                      onClick={() => handleDelete([record.id])}
+                    >
+                      {t('table.actions.delete')}
+                    </Button>
+                  );
+                },
               },
             ]}
+            rowSelection={{
+              selectedRowKeys: selectedKeys,
+              onChange: (selectedRowKeys) => {
+                setSelectedKeys(selectedRowKeys);
+              },
+            }}
             {...tableProps}
           />
         </Card>
